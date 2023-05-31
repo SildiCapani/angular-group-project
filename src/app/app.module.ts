77bf146fb36123d9adee54ba49fa488691e3a9bf
Sildi Capani <88593303+SildiCapani@users.noreply.github.com>
2023-05-31 21:17:55 +0200

    created-comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalenderComponent } from './calender/calender.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalenderComponent,
+    TaskFormComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
